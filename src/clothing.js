dad5b4d1996fd02890a231e7bf6000901d5f413f
Nick Jackson <nickdjackson@msn.com>
2022-10-22 18:15:51 -0400

    comments

--- src/clothing.js
+++ src/clothing.js
@@ -21,3 +21,27 @@ export function getAllClothing(req, res) {
         })
         .catch(err => res.send.status(500).send({success: false, message: err}))
 }
+
+export async function updateClothing(req, res) {
+    const { uid } = req.params
+    const db = dbConnect()
+    const doc = await db.collection('clothing').doc(uid).update(req.body)
+        .catch(err => res.status(500).send({ success: false, message: err }))
+    res.status(202).send({ success: true, message: 'Clothing Updated ' + doc.id })
+}
+
+export async function deleteClothing(req, res) {
+    const { uid } = req.params
+    const db = dbConnect()
+    const doc = await db.collection('clothing').doc(uid).delete()
+        .catch(err => res.status(500).send({ success: false, message: err }))
+    res.status(202).send({ success: true, message: 'Clothing Deleted' })
+}
+
+export async function getOneClothing(req, res) {
+    const { uid } = req.params
+    const db = dbConnect()
+    const doc = await db.collection('clothing').doc(uid).get()
+        .catch(err => res.status(500).send({ success: false, message: err }))
+        res.status(202).send({ success: true, message: doc.data()})
+}
